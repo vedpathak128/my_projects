cd140c992ac110607969585b81fea4a23da5ec67
Ved Pathak <vedpathak128@gmail.com>
2026-09-10 17:40:26 +0530

    Add files via upload
    this is a upload of my self made chatbot using a whatsapp id

---
+++ chat_bot/openai.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+ 
+client = OpenAI(
+  api_key="<Your Key Here>",
+)
+
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a person named harry who speaks hindi as well as english. He is from India and is a coder. You analyze chat history and respond like Harry"},
+    {"role": "user", "content": command}
+  ]
+)
+
+print(completion.choices[0].message.content)
